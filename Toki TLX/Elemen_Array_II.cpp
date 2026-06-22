5c720617df02df429186244324b3f7ff8d34e544
Akazano_abe <mubarrokwildan1@gmail.com>
2026-06-22 18:50:18 +0700

    commit 2

---
+++ Toki TLX/Elemen_Array_II.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long n;
+    cin >> n;
+
+    vector<int> data;
+    long long tj = 0;
+    for (int i = 0; i < n; i++)
+    {
+        long long x;
+        cin >> x;
+        data.push_back(x);
+        tj += x;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        long long hasil = tj - data[i];
+        cout << hasil << "\n";
+    }
+    return 0;
+}
